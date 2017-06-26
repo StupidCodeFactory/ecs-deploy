520277a7020e787592e7ab1e8bbb7d474a7f87ad
yann marquet <ymarquet@gmail.com>
2017-06-26 22:05:58 +0100

    add some cluster manipulation

--- main.rb
+++ main.rb
@@ -1,14 +1,29 @@
 require 'bundler/setup'
 require 'aws-sdk'
 require 'pp'
+require 'optparse'
 
 class ECSDeploy
 
-  def cluster
-    pp ecs
-    pp clusters
+  def initialize(cluster_name)
+    self.cluster_name = cluster_name
   end
+
+  def cluster
+    @cluster ||= ecs.create_cluster(cluster_name: cluster_name).cluster
+  end
+
+  def delete_cluster
+    ecs.delete_cluster(cluster: cluster_name)
+  end
+  
+  def register_task_definition(task_definition)
+    pp ecs.list_task_definition_families
+    ecs.register_task_definition(task_definition)
+  end
+  
   private
+  attr_accessor :cluster_name
   
   def clusters
     @clusters ||= ecs.list_clusters
@@ -17,6 +32,19 @@ class ECSDeploy
   def ecs
     @ecs ||= Aws::ECS::Client.new(region: 'eu-west-1')
   end
+
+  def instance
+    @instance ||= ec2.create_instances(instance_type: 't1.micro')
+  end
+
+  def ec2
+    @ec2 ||= Aws::EC2::Resource.new(client:  Aws::EC2::Client.new(region: 'eu-west-1'))
+  end
 end
 
 
+if __FILE__ == $0
+
+  pp ENV.inspect
+  
+end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,18 +1,24 @@
-
 require 'spec_helper'
 require 'byebug'
 
 RSpec.describe ECSDeploy, vcr: { cassette_name: 'cluster_steps', record: :new_episodes}  do
 
   let(:ecs_client) { Aws::ECS::Client.new(region: 'eu-west-1') }
+  let(:cluster_name) { 'ecs-deploy-wercker-step' }
+  let(:ec2) { Aws::EC2::Client.new(region: 'eu-west-1') }
+  let(:resource) { Aws::EC2::Resource.new(client: ec2) }
   
-  it 'initialise an AWS client' do
-    # expect(Aws::ECS::Client).to receive(:new).with(region: 'eu-west-1').and_return(ecs_client)
-    subject.cluster
+  subject { described_class.new(cluster_name) }
+  after do
+    subject.delete_cluster
   end
   
-  it 'creates a cluster' do
-    # expect(subject.cluster).to eq()
+  describe '#cluster' do
+    it 'creates a cluster' do
+      expect(subject.cluster.cluster_name).to eq(cluster_name)
+      expect(subject.cluster.cluster_arn).to eq('arn:aws:ecs:eu-west-1:466565482925:cluster/ecs-deploy-wercker-step')
+    end
   end
-  
+
 end
+
